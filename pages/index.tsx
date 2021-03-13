7071381dd82f273e27b482bbe10b267f53c6451b
mndiaz90 <mayler900123@gmail.com>
2021-03-13 17:12:13 -0300

    tirado o router do index

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import styles from '../styles/Home.module.css'
 import Dashboard from './dashboard'
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Router } from 'next/dist/client/router';
 
 export default function Home() {
   return (
